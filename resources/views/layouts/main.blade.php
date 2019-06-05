7ea8221f87df13c0c48670477e2ade1754b331dc
Amel Nurkic <anurkic@capeannenterprises.com>
2019-06-05 14:27:52 +0200

    Automatic AdSense implementation

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,14 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <!-- AdSense -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-3529888203879797",
+            enable_page_level_ads: true
+        });
+    </script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
